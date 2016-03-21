ab6b2641dfd4824da61db1e3d2072be6e1ca47a0
Carlos Borges <caborges3@gmail.com>
2016-03-21 15:35:10 -0700

    add css classes for cats

--- js/script.js
+++ js/script.js
@@ -6,16 +6,34 @@ var red = document.getElementById('redBox')
 var blue = document.getElementById('blueBox')
 var yellow = document.getElementById('yellowBox')
 var pink = document.getElementById('pinkBox')
+var start = document.getElementById('start')
+var reset = document.getElementById('reset')
 
 // set click handlers
 red.addEventListener("click", press)
 blue.addEventListener("click", press)
 yellow.addEventListener("click", press)
 pink.addEventListener("click", press)
+start.addEventListener("click", startGame)
+reset.addEventListener("click", resetGame)
 
 // function for user button press
 function press() {
   console.log(this.innerHTML)
+}
+
+// start game
+function startGame() {
+  lightUpBox();
+}
+
+function lightUpBox() {
+  console.log("lighting up box");
+}
+
+// reset game
+function resetGame() {
+  console.log('one day this will reset the game.')
 }
 
 // function for computer generate random number. Indicate which
@@ -23,7 +41,7 @@ function press() {
 function randomMove(move) {
   var randomNumber = Math.random();
   if (randomNumber < .25) {
-    return red;
+    return red; //indicate selection
   } else if (randomNumber < .50) {
     return blue;
   } else if (randomNumber < .75) {
@@ -33,8 +51,18 @@ function randomMove(move) {
   }
 }
 
+// questionable code starts here!
 // user clicks corresponding button
+function getPlayerMove(move) {
+  var move = move || getInput();
+  return move;
+}
 
+function checkWinner(playerMove, computerMove) {
+  if (playerMove===computerMove) {
+    //proceed to next round.
+  }
+}
 
 
 // computer adds the first button to the sequence [Array?] and picks
