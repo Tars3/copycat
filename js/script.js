722beebaab23c2cfa503bc5ea348b61775a689a2
Carlos Borges <caborges3@gmail.com>
2016-03-22 15:46:38 -0700

    Adding beginning of game logic

--- js/script.js
+++ js/script.js
@@ -2,15 +2,35 @@ console.log("Testing, One, Two")
 
 var speed = 650;
 var round = 1; // doubles as sequence length!
+var turn
 
 // create clickable dom elements for 4 buttons
-
 var redEl    = document.getElementById('redBox')
 var blueEl   = document.getElementById('blueBox')
 var yellowEl = document.getElementById('yellowBox')
 var pinkEl   = document.getElementById('pinkBox')
 var startButton = document.getElementById('start')
 var resetButton = document.getElementById('reset')
+
+round = 1
+//computerMove == array[totalRounds]
+
+var computerMove = []
+
+moves = ["red", "yellow", "blue", "pink"]
+elements = {
+  "red": redEl,
+  "blue": blueEl,
+  "yellow": yellowEl,
+  "pink": pinkEl,
+}
+
+// elementIdMoves = {
+//   "redBox": "red",
+
+// }
+// elementToLightUp = elements[move];
+// getMoveFromElementId()
 
 // set click handlers
 redEl.addEventListener("click", press)
@@ -20,15 +40,12 @@ pinkEl.addEventListener("click", press)
 startButton.addEventListener("click", startGame)
 resetButton.addEventListener("click", resetGame)
 
-// function for user button press
-function press() {
-  console.log(this.innerHTML)
-}
-
 // start game
 function startGame() {
-  var boxEl = randomMove();
-  lightUpBox(boxEl);
+   var boxEl = randomMove();
+   lightUpBox(boxEl);
+  computerMove.push(boxEl);
+  console.log(computerMove)
 }
 
 function lightUpBox(box) {
@@ -44,6 +61,11 @@ function lightUpBox(box) {
 function dimBox(box) {
   box.classList.remove("lit-up")
   box.classList.remove("meow")
+}
+
+// function for user button press
+function press(box) {
+
 }
 
 // reset game
@@ -68,20 +90,21 @@ function randomMove(move) {
 
 // questionable code starts here!
 // user clicks corresponding button
-function getPlayerMove(move) {
-  var move = move || getInput();
-  return move;
-}
 
-function checkWinner(playerMove, computerMove) {
-  if (playerMove===computerMove) {
-    //proceed to next round.
-  }
-}
+// if player clicks are less than sequence length for round computer waits.
+
+// function checkWinner(playerMove, computerMove) {
+//   if (playerMove===computerMove) {
+//     //proceed to next round.
+//   }
+// }
 
 
 // computer adds the first button to the sequence [Array?] and picks
-// a new random button for the next round.
+// a new random button for the next round.(wrong)
+
+// computer pregenerates random sequence of buttons at beginning of the
+// game and then shows those moves as the round count goes up.
 
 
 
