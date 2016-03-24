dabccab0a9202c5828cfba5e1df09818e9d1db42
Carlos Borges <caborges3@gmail.com>
2016-03-23 19:39:38 -0700

    begin user input

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 // console.log("Testing, One, Two")
 var speed = 650;
 var round = 1; // doubles as sequence length!
+var turn = 0;
+var computerMove = [];
+var playerMove = [];
+var playerSeq = [];
+var correctSeq = [];
+var lightSeq = [];
 
 // create clickable dom elements for 4 buttons
 var redEl    = document.getElementById('red')
@@ -18,11 +24,6 @@ pinkEl.addEventListener("click", press)
 startButton.addEventListener("click", startGame)
 resetButton.addEventListener("click", resetGame)
 
-//computerMove == array[totalRounds]
-var computerMove = []
-var playerMove = []
-var correctSeq = []
-
 // moves = ["red", "yellow", "blue", "pink"]
 // elements = {
 //   "red": redEl,
@@ -31,12 +32,29 @@ var correctSeq = []
 //   "pink": pinkEl,
 // }
 
+// function for computer generate random number. Indicate which
+// button was "selected".
+function randomMove(move) {
+  var randomNumber = Math.random();
+  if (randomNumber < .25) {
+    return redEl; //indicate selection
+  } else if (randomNumber < .50) {
+    return blueEl;
+  } else if (randomNumber < .75) {
+    return yellowEl;
+  } else {
+    return pinkEl;
+  }
+}
+
 // start game
 function startGame() {
    var boxEl = randomMove();
-   lightUpSequence(boxEl);
-  computerMove.push(boxEl);
-  console.log(computerMove);
+  correctSeq.push(boxEl.id);
+  lightSeq.push(boxEl)
+  console.log(boxEl);  // sub from computerMove
+  lightUpSequence(lightSeq);
+  turn = turn + 1;
 }
 
 // for loop for round numbers
@@ -45,8 +63,8 @@ for (var i = 0; i >= round.length; i++) {
   console.log(computerMove)
 }
 
-function lightUpSequence(sequence) {
-  sequence.forEach(function (box, i) {
+function lightUpSequence(correctSeq) { //sub for sequence
+  correctSeq.forEach(function (box, i) {  //sub for sequence.forEach
     setTimeout(function() {
       console.log(box)
       flashBox(box);
@@ -58,11 +76,35 @@ function lightUpSequence(sequence) {
 // undefined
 // lightUpSequence(seq)
 
-// make sequence
-function makeSeq() {
+// add to sequence
+function addSeq() {
   if(correctSeq.length !== round) {
     correctSeq.push(randomMove());
   }
+}
+
+// playerSeq
+function playerTurn() {
+  if(playerSeq == correctSeq && playerSeq.length == correctSeq) {
+    turn = turn +1
+  } else if (playerSeq == correctSeq && playerSeq.length != correctSeq) {
+    // wait?
+  } else {
+    alert("You LOSE, You get NOTHING!");
+    round = 0;
+  }
+}
+
+// function for user button press
+function press(boxEl) {
+  playerSeq.push(boxEl.target.id);
+  console.log(boxEl);
+  flashBox(document.getElementById(boxEl.target.id));
+  turn++;
+}
+
+if(playerSeq.join == correctSeq.join && playerSeq.length != 0) {
+  startGame();
 }
 
 function play () {
@@ -74,10 +116,6 @@ function play () {
     }
   }
   else {alert("DYSIODFSJSDL:KFJ:OSIDFJ YOU WIN!")}
-}
-
-function checkLose() {
-
 }
 
 function winRound() {
@@ -105,31 +143,12 @@ function flashBox(box) {
   }, speed);
 }
 
-
-// function for user button press
-function press(box) {
-
-}
-
 // reset game
 function resetGame() {
   console.log('one day this will reset the game.')
+  round = 0
 }
 
-// function for computer generate random number. Indicate which
-// button was "selected".
-function randomMove(move) {
-  var randomNumber = Math.random();
-  if (randomNumber < .25) {
-    return redEl; //indicate selection
-  } else if (randomNumber < .50) {
-    return blueEl;
-  } else if (randomNumber < .75) {
-    return yellowEl;
-  } else {
-    return pinkEl;
-  }
-}
 
 // questionable code starts here!
 // user clicks corresponding button
@@ -142,12 +161,8 @@ function randomMove(move) {
 //   }
 // }
 
-
 // computer adds the first button to the sequence [Array?] and picks
 // a new random button for the next round.(wrong)
 
 // computer pregenerates random sequence of buttons at beginning of the
 // game and then shows those moves as the round count goes up.
-
-
-
